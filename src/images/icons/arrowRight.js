bfdb875f91cfa41d290d94349a83f4843633ef55
Maxime Vanden Haezevelde <mvandenhaezevelde@gmail.com>
2020-09-02 10:21:49 +0200

    correction css

--- src/images/icons/arrowRight.js
+++ src/images/icons/arrowRight.js
@@ -1,28 +1,26 @@
 import React from "react";
 
-function ArrowRight(
+const ArrowRight = ({
   size = 19,
   color = "#FAFAFA",
   strokeWidth = "1.5",
   fill = "none",
   strokeLinecap = "round",
-  strokeLinejoin = "round"
-) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={size}
-      height={size}
-      viewBox="0 0 24 24"
-      fill={fill}
-      stroke={color}
-      strokeWidth={strokeWidth}
-      strokeLinecap={strokeLinecap}
-      strokeLinejoin={strokeLinejoin}
-    >
-      <path d="M5 12h13M12 5l7 7-7 7" />
-    </svg>
-  );
-}
+  strokeLinejoin = "round",
+}) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill={fill}
+    stroke={color}
+    strokeWidth={strokeWidth}
+    strokeLinecap={strokeLinecap}
+    strokeLinejoin={strokeLinejoin}
+  >
+    <path d="M5 12h13M12 5l7 7-7 7" />
+  </svg>
+);
 
 export default ArrowRight;
--- src/images/icons/eye.js
+++ src/images/icons/eye.js
@@ -1,22 +1,20 @@
 import React from "react";
 
-function Eye(size = 19, color = "#FAFAFA", strokeWidth = "1.5") {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={size}
-      height={size}
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke={color}
-      strokeWidth={strokeWidth}
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
-      <circle cx="12" cy="12" r="3"></circle>
-    </svg>
-  );
-}
+const Eye = ({ size = 19, color = "#FAFAFA", strokeWidth = "1.5" }) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke={color}
+    strokeWidth={strokeWidth}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  >
+    <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+    <circle cx="12" cy="12" r="3"></circle>
+  </svg>
+);
 
 export default Eye;
